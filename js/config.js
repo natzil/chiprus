de813e52237325da00c610d997241b4b1fed3350
natzil <132285461+natzil@users.noreply.github.com>
2026-07-04 17:45:02 +0300

    Delete js directory

--- js/config.js
+++
@@ -1,4 +0,0 @@
-window.APP_CONFIG = {
-  GOOGLE_APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyuQA5VKzvXeAy2NUtuqavnELHIMEsWZD2HcAYzVOs9C5uErlOfNpYL-TIVmOjfB4vd2Q/exec',
-  DEMO_MODE: false
-};
